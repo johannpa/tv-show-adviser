b138de240b7708731d32bd14cfcc0262794d30b8
johannpa <johann_paque@hotmail.fr>
2024-04-23 20:00:19 +0200

    Design the app and use style

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,23 @@
 import React from "react";
 import "./global.css";
+import style from "./style.module.css";
 
 export function App() {
-  return <div>Hello world</div>;
+  return (
+    <div className={style.main_container}>
+      <div className={style.header}>
+        <div className="row">
+          <div className="col-4">
+            <div>Logo</div>
+            <div>Subtitle</div>
+          </div>
+          <div className="col-sm-12 col-md-4">
+            <input style={{ width: "100%" }} type="text" />
+          </div>
+        </div>
+      </div>
+      <div className={style.tv_show_detail}>Detail</div>
+      <div className={style.recommendations}>Recommendations</div>
+    </div>
+  );
 }
