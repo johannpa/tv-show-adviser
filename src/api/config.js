b79c846a154fe754b4b0b1cc104ea46d2ef43893
johannpa <johann_paque@hotmail.fr>
2024-05-01 17:34:08 +0200

    Create a new file for the configuration and put the constants in it

---
+++ src/api/config.js
@@ -0,0 +1,4 @@
+const BASE_URL = "https://api.themoviedb.org/3/";
+const API_KEY_PARAM = "?api_key=";
+
+export { BASE_URL, API_KEY_PARAM };
--- src/api/tv-show.js
+++ src/api/tv-show.js
@@ -1,6 +1,4 @@
 import axios from "axios";
-const BASE_URL = "https://api.themoviedb.org/3/";
-const API_KEY_PARAM = "?api_key=";
 
 export class TVShowAPI {
   static async fetchPopulars() {
