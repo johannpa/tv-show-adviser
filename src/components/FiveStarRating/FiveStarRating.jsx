7c250536b0e463e82b967186c8a239c121a241a0
johannpa <johann_paque@hotmail.fr>
2024-05-04 10:20:45 +0200

    Erase the stars' rating test and create the real rating

--- src/components/FiveStarRating/FiveStarRating.jsx
+++ src/components/FiveStarRating/FiveStarRating.jsx
@@ -2,11 +2,12 @@ import s from "./style.module.css";
 import { StarFill, Star as StarEmpty, StarHalf } from "react-bootstrap-icons";
 
 export function FiveStarRating({ rating }) {
-    return <div>
-        <StarFill/>
-        <StarFill/>
-        <StarFill/>
-        <StarHalf/>
-        <StarEmpty/>
-    </div>
+    const starList = [];
+    const starFillCount = Math.floor(rating);
+    const hasStarHalf = rating - starFillCount >= 0.5;
+    const emptyStarCount = 5 - starFillCount - (hasStarHalf ? 1 : 0);
+    console.log(starFillCount, hasStarHalf, emptyStarCount);
+
+
+    return <div>{starList}</div>
 }
