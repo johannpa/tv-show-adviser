fbfcfdf1f306f6e7547283fe8bfaa9ec5794bf40
johannpa <johann_paque@hotmail.fr>
2024-05-04 09:11:48 +0200

    Test the react-boostrap-icons star

--- src/components/FiveStarRating/FiveStarRating.jsx
+++ src/components/FiveStarRating/FiveStarRating.jsx
@@ -1,5 +1,12 @@
 import s from "./style.module.css";
+import { StarFill, Star as StarEmpty, StarHalf } from "react-bootstrap-icons";
 
 export function FiveStarRating({ rating }) {
-    return <div>*****</div>
+    return <div>
+        <StarFill/>
+        <StarFill/>
+        <StarFill/>
+        <StarHalf/>
+        <StarEmpty/>
+    </div>
 }
