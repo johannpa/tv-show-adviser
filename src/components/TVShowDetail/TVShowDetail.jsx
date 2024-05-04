2484d4c5bb95792f1dffdb723595c0378e54b6b0
johannpa <johann_paque@hotmail.fr>
2024-05-04 09:30:35 +0200

    Fix the rating for TVShowDetail component and add its css

--- src/components/TVShowDetail/TVShowDetail.jsx
+++ src/components/TVShowDetail/TVShowDetail.jsx
@@ -2,13 +2,13 @@ import { FiveStarRating } from "../FiveStarRating/FiveStarRating";
 import s from "./style.module.css";
 
 export function TVShowDetail({tvShow}){
-    const rating = tvShow.vote_average / 2;
+    const rating = tvShow.vote_average.toFixed(1) / 2;
 
     return <div>
         <div className={s.title}>{tvShow.name}</div>
         <div className={s.rating_container}>
             <FiveStarRating rating={rating}/>
-            <div className={s.rating}>{rating}/5</div>
+            <div className={s.rating}>{rating}</div>
         </div>
         <div className={s.overview}>{tvShow.overview}</div>
 
