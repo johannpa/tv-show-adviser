62e4f76f23785e16bd6d39c5b72a9d8fb38776ce
johannpa <johann_paque@hotmail.fr>
2024-05-04 08:08:35 +0200

    Create className for the different elements of TVShowDetail

--- src/components/TVShowDetail/TVShowDetail.jsx
+++ src/components/TVShowDetail/TVShowDetail.jsx
@@ -2,8 +2,8 @@ import s from "./style.module.css";
 
 export function TVShowDetail({tvShow}){
     return <div>
-        <div>{tvShow.name}</div>
-        <div>{tvShow.vote_average/2}</div>
-        <div>{tvShow.overview}</div>
+        <div className={s.title}>{tvShow.name}</div>
+        <div className={s.rating}>{tvShow.vote_average/2}</div>
+        <div className={s.overview}>{tvShow.overview}</div>
     </div>
 }
