a2d6b99507131669db48fc1b6269373f8dc83b37
johannpa <johann_paque@hotmail.fr>
2024-05-08 16:59:07 +0200

    Fill the component and its css file

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,5 +1,15 @@
 import s from './style.module.css'
+import { Search as SearchIcon } from "react-bootstrap-icons"
 
 export function SearchBar(props) {
-    return <div></div>;
+    return (
+      <>
+        <SearchIcon size={27} className={s.icon}/>
+        <input
+          type="text"
+          placeholder="Search a tv show you may like"
+          className={s.input}
+        />
+      </>
+    );
 }
