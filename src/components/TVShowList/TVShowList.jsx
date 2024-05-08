26bbf5d913bdace7c86ad28ce4aa16d8223278bd
johannpa <johann_paque@hotmail.fr>
2024-05-08 16:12:58 +0200

    Apply recommendations

--- src/components/TVShowList/TVShowList.jsx
+++ src/components/TVShowList/TVShowList.jsx
@@ -1,7 +1,7 @@
 import { TVShowListItem } from "../TVShowListItem/TVShowListItem";
 import s from "./style.module.css";
 
-export function TVShowList({ tvShowList }) {
+export function TVShowList({ tvShowList, onClickItem }) {
     return (
     <>
         <div className={s.title}>You may also like:</div>
@@ -9,7 +9,7 @@ export function TVShowList({ tvShowList }) {
             {tvShowList.map((tvShow) => {
                 return (
                     <span key={tvShow.id} className={s.tv_show_list_item}>
-                        <TVShowListItem tvShow={tvShow} onClick={() =>""} />
+                        <TVShowListItem onClick={onClickItem} tvShow={tvShow} />
                     </span>
                 );
             })}
